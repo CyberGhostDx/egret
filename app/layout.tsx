c2c66c31f9b0260828b2edd82693e9b1e97699fe
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-02-18 18:34:45 +0700

    chore: remove unused file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Sans_Thai } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const ibmSans = IBM_Plex_Sans_Thai({
+  variable: "--font-ibm",
+  subsets: ["latin", "thai"],
+  weight: ["400", "500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${ibmSans.variable} ${ibmSans.className}  antialiased`}>
         {children}
       </body>
     </html>
