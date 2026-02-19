946cc0ae53214db5b3f61f248aa1b21abd053cc2
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-02-19 17:57:12 +0700

    feat: add proxy auth middleware

---
+++ middleware/auth.middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { authClient } from "@/lib/auth-client";
+
+export default async function authMiddleware(req: NextRequest) {
+  const { data: session } = await authClient.getSession({
+    fetchOptions: {
+      headers: {
+        cookie: req.headers.get("cookie") || "",
+      },
+    },
+  });
+
+  if (!session?.user) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
---
+++ proxy.ts
@@ -0,0 +1,10 @@
+import { NextRequest } from "next/server";
+import authMiddleware from "./middleware/auth.middleware";
+
+export async function proxy(request: NextRequest) {
+  return await authMiddleware(request);
+}
+
+export const config = {
+  matcher: ["/((?!login|api|_next/static|_next/image|favicon.ico).*)"],
+};
