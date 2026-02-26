6633ecd8aa19883f674c9bf34ffab11ccac9f3a2
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-02-26 13:49:09 +0700

    feat: add axios interceptor for intercept status 401 unauthoriztion

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -21,4 +21,19 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (
+        typeof window !== "undefined" &&
+        window.location.pathname !== "/login"
+      ) {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
