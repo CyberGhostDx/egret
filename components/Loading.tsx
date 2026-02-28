21f89fb933e04baef9ada69776ed2c5e2272a9c6
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-02-28 12:07:40 +0700

    fix: heroui spinner hydration failed

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,16 @@
+"use client";
 import { Spinner } from "@heroui/react";
+import { useEffect, useState } from "react";
 
 const Loading = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <div className="flex h-full w-full items-center justify-center">
       <Spinner />
