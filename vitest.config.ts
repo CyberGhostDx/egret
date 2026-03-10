edfcf41a54095bb4cd67c33976f2b466b4da2911
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-03-10 10:57:14 +0700

    chore: setup testing environment

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+    include: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+    exclude: ["node_modules", ".next"],
+    globals: true,
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+});
